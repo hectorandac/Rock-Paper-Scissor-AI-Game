21301b569b58803f9f1fc1f00d5604919ea42826
Hector Andres Acosta Pozo <hector.acosta.pozo@outlook.com>
2025-11-22 14:23:57 +0000

    Refactor Dockerfile and server.py for improved logging and error handling; update docker-compose.yml for network configuration; modify envoy.yaml for connection timeout and address.

--- backend/server.py
+++ backend/server.py
@@ -8,23 +8,41 @@ import predictor_pb2_grpc
 import cv2 as cv
 from yolov6.core.inferer import Inferer
 
-
+# Initialize the Inferer
 inferer = Inferer("", "./weights/yHand6.pt", 0, "./data.yaml", [160, 160], False)
 class_names = ['rock', 'paper', 'scissors']
 
+# Set up logging
+logging.basicConfig(level=logging.INFO, format='%(asctime)s - %(levelname)s - %(message)s')
+
 class Predictor(predictor_pb2_grpc.PredictServicer):
     def PredictRoPaSc(self, request, context):
+        logging.info("Received request for prediction")
+        
+        # Decode the base64 image
         imageB64 = request.base64image
-        base64_decoded = base64.b64decode(imageB64)
-        image = np.asanyarray(bytearray(base64_decoded), dtype=np.uint8)
-        image = cv.imdecode(image, cv.IMREAD_COLOR)
-        output2 = inferer.infer2(image)
         try:
-            output = output2.cpu().numpy()[0]
-            return predictor_pb2.PredictionReply(predictedClass=int(output[5]), confidence=output[4])
-        except:
+            # logging.info("Decoding base64 image")
+            base64_decoded = base64.b64decode(imageB64)
+            image = np.asanyarray(bytearray(base64_decoded), dtype=np.uint8)
+            image = cv.imdecode(image, cv.IMREAD_COLOR)
+            # logging.info("Image decoded successfully")
+        except Exception as e:
+            # logging.error(f"Failed to decode the image: {e}")
             return predictor_pb2.PredictionReply(predictedClass=-1, confidence=0)
 
+        # Perform inference
+        try:
+            # logging.info("Running inference on the image")
+            output2 = inferer.infer2(image)
+            output = output2.cpu().numpy()[0]
+            predicted_class = int(output[5])
+            confidence = output[4]
+            # logging.info(f"Inference successful. Predicted class: {predicted_class}, Confidence: {confidence}")
+            return predictor_pb2.PredictionReply(predictedClass=predicted_class, confidence=confidence)
+        except Exception as e:
+            logging.error(f"Inference failed: {e}")
+            return predictor_pb2.PredictionReply(predictedClass=-1, confidence=0)
 
 def serve():
     port = '50051'
@@ -32,10 +50,8 @@ def serve():
     predictor_pb2_grpc.add_PredictServicer_to_server(Predictor(), server)
     server.add_insecure_port('[::]:' + port)
     server.start()
-    print("Server started, listening on " + port)
+    logging.info(f"Server started, listening on port {port}")
     server.wait_for_termination()
 
-
 if __name__ == '__main__':
-    logging.basicConfig()
     serve()
